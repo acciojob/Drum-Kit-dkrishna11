ccedee36c2693d9e9e7631780fb14bba88062dcd
dkrishna11 <116519809+dkrishna11@users.noreply.github.com>
2023-06-10 13:25:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+function playSound(e) {
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
+  
+  if (!audio) return;
+  
+  audio.currentTime = 0; // Rewind to the start
+  audio.play();
+  
+  key.classList.add('playing');
+}
+
+function removeTransition(e) {
+  if (e.propertyName !== 'transform') return;
